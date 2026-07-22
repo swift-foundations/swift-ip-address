d5f7fda7befe099e5362cd8f0fa9d498b7a6760f
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-07-23 00:34:49 +0200

    Add provider-neutral IP address sum

--- Tests/IP Address Tests/IP Address Tests.swift
+++ Tests/IP Address Tests/IP Address Tests.swift
@@ -17,4 +17,13 @@ struct `IP Address Tests` {
     @Test
     func `IP Address module re-exports IPv4_Standard and IPv6_Standard`() {
     }
+
+    @Test
+    func `Address preserves canonical payloads and provider order`() {
+        let v6 = IPv6.Address(0, 0, 0, 0, 0, 0, 0, 1)
+        let v4 = IPv4.Address(rawValue: 0x7f00_0001)
+        let addresses: [IP.Address] = [.v6(v6), .v4(v4)]
+
+        #expect(addresses == [.v6(v6), .v4(v4)])
+    }
 }
